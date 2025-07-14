a1d8bbed068c3e5c505800b5f59874a97ee3b754
Javier Sancho <sanchopej@gmail.com>
2025-07-14 14:13:32 +0200

    He añadido a update lo que debe hacer cuando pulso los botones de goTown, goCave o goStore, y en sus funciones le he pasado la funcion update con la posicion de cada uno de ellos como parametro

--- juegoRol/script.js
+++ juegoRol/script.js
@@ -79,19 +79,26 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function update(location) {
-
+  monsterStats.style.display = "none";
+  button1.innerText = location["button text"][0];
+  button2.innerText = location["button text"][1];
+  button3.innerText = location["button text"][2];
+  button1.onclick = location["button functions"][0];
+  button2.onclick = location["button functions"][1];
+  button3.onclick = location["button functions"][2];
+  text.innerHTML = location.text;
 }
 
 function goTown() {
-
+  update(locations[0]);
 }
 
 function goStore() {
-
+  update(locations[1]);
 }
 
 function goCave() {
-
+  update(locations[2]);
 }
 
 function buyHealth() {
